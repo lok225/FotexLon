69137c2112994c818d163ce4fc9d8a51b68a785f
loklak5 <martinlok@icloud.com>
2016-07-22 15:19:08 +0200

    Initial Commit

---
+++ Føtex Løn/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Føtex Løn
+//
+//  Created by Martin Lok on 22/07/2016.
+//  Copyright © 2016 Martin Lok. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
